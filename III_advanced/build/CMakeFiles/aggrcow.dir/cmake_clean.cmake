c5df29dded23c2a87e42a1c0e499b6421b167ae9
macaljan_LinNtb <macaljan@fel.cvut.cz>
2024-02-23 12:58:37 +0100

    adding new files from contest I

---
+++ III_advanced/build/CMakeFiles/aggrcow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aggrcow.dir/1_6_aggrcow.cpp.o"
+  "aggrcow"
+  "aggrcow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aggrcow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
